32d994594f568495467928afc5ab991ba5001790
dxkite <dxkite@163.com>
2020-12-05 08:52:27 +0800

    添加：读取Buffer

---
+++ buffer/reader.go
@@ -0,0 +1,81 @@
+package buffer
+
+import (
+	"errors"
+	"io"
+)
+
+type RewindReader interface {
+	io.Reader
+	Rewind() error
+}
+
+// 预读取的数据大于缓存区的数据不能回退
+var ErrRewindSize = errors.New("rewind size error")
+
+type rewindReader struct {
+	r   io.Reader // 读取院
+	buf []byte    // 缓存的位置
+	off int       // 读取到的位置
+	wd  int       // 缓冲区使用的大小
+	max int       // 缓冲区大小
+}
+
+// 创建可缓冲
+func NewRewindReaderSize(r io.Reader, size int) RewindReader {
+	return &rewindReader{
+		r:   r,
+		buf: make([]byte, size),
+		off: 0,
+		wd:  0,
+		max: size,
+	}
+}
+
+// 读取数据
+// 如果缓冲区有数据，读取缓冲区数据
+// 如果超出缓存区数据，缓冲区失效，直接读取数据
+func (rr *rewindReader) Read(p []byte) (n int, err error) {
+	// 无缓冲区/缓冲区失效
+	if len(rr.buf) == 0 {
+		return rr.r.Read(p)
+	}
+	// 当前数据重置了读取指针
+	if rr.off < rr.wd {
+		lp := len(p)
+		n := 0
+		if rr.off+lp > rr.wd {
+			n = copy(p, rr.buf[rr.off:rr.wd])
+		} else {
+			n = copy(p, rr.buf[rr.off:rr.off+lp])
+		}
+		rr.off += n
+		return n, err
+	}
+	// 从数据源读取
+	if n, er := rr.r.Read(p); er != nil {
+		return 0, er
+	} else {
+		// 当前读取的数据大于缓冲区的数据
+		if n+rr.off > rr.max {
+			// 缓冲区失效
+			// 降级为普通读取
+			rr.buf = rr.buf[0:0]
+			return n, er
+		}
+		// 将数据复制到缓冲区
+		copy(rr.buf[rr.off:], p)
+		rr.off += n
+		rr.wd += n
+		return n, nil
+	}
+}
+
+// 重置读取位置，从头读取
+func (rr *rewindReader) Rewind() error {
+	if len(rr.buf) != rr.max {
+		return ErrRewindSize
+	}
+	rr.off = 0
+	return nil
+}
---
+++ buffer/reader_test.go
@@ -0,0 +1,64 @@
+package buffer
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func Test_rewindReader_Read(t *testing.T) {
+	buf1 := NewRewindReaderSize(bytes.NewReader([]byte("dxkite12345")), 6)
+	readNEqual(buf1, 2, "dx", nil, t)
+	readNEqual(buf1, 4, "kite", nil, t)
+	readNEqual(buf1, 5, "12345", nil, t)
+	if e := buf1.Rewind(); e != ErrRewindSize {
+		t.Error("want", ErrRewindSize, "got", e)
+	}
+	buf2 := NewRewindReaderSize(bytes.NewReader([]byte("dxkite12345678910")), 6)
+	readNEqual(buf2, 2, "dx", nil, t)
+	if e := buf2.Rewind(); e != nil {
+		t.Error("want", ErrRewindSize, "got", e)
+	}
+	readFullNEqual(buf2, 6, "dxkite", nil, t)
+	if e := buf2.Rewind(); e != nil {
+		t.Error("want", ErrRewindSize, "got", e)
+	}
+	readFullNEqual(buf2, 6, "dxkite", nil, t)
+	if e := buf2.Rewind(); e != nil {
+		t.Error("want", ErrRewindSize, "got", e)
+	}
+	readFullNEqual(buf2, 11, "dxkite12345", nil, t)
+	if e := buf2.Rewind(); e != ErrRewindSize {
+		t.Error("want", ErrRewindSize, "got", e)
+	}
+	readFullNEqual(buf2, 6, "678910", nil, t)
+	if e := buf2.Rewind(); e != ErrRewindSize {
+		t.Error("want", ErrRewindSize, "got", e)
+	}
+	readFullNEqual(buf2, 6, "", io.EOF, t)
+
+	buf3 := NewRewindReaderSize(bytes.NewReader([]byte("dxkite12345")), 6)
+	readNEqual(buf3, 100, "dxkite12345", nil, t)
+}
+
+func readNEqual(r io.Reader, n int, rd string, err error, t *testing.T) {
+	buf := make([]byte, n)
+	rs, e := r.Read(buf)
+	if e != err {
+		t.Error("read", n, "real read", rs, "want error", err, "got", e)
+	}
+	if string(buf[:rs]) != rd {
+		t.Error("read", n, "real read", rs, "want", rd, "got", string(buf[:rs]))
+	}
+}
+
+func readFullNEqual(r io.Reader, n int, rd string, err error, t *testing.T) {
+	buf := make([]byte, n)
+	rs, e := io.ReadFull(r, buf)
+	if e != err {
+		t.Error("read", n, "real read", rs, "want error", err, "got", e)
+	}
+	if string(buf[:rs]) != rd {
+		t.Error("read", n, "real read", rs, "want", rd, "got", string(buf[:rs]))
+	}
+}
