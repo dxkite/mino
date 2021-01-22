b9d35a653d812626d77a24dbe32e9bd9eda5d611
dxkite <dxkite@163.com>
2021-01-22 21:11:00 +0800

    调整：api接口路径

--- server/server.go
+++ server/server.go
@@ -19,15 +19,20 @@ func NewServer(tsp *transport.Transporter) *Server {
 
 func (s *Server) Serve() error {
 	c := s.tsp.Config
+	root := config.GetConfigFile(c, c.StringOrDefault(mino.KeyWebRoot, "www"))
 	mux := http.NewServeMux()
 	mux.Handle(mino.PathMinoPac, monkey.NewPacServer(c))
-	root := config.GetConfigFile(c, c.StringOrDefault(mino.KeyWebRoot, "www"))
 	mux.Handle("/check-update", &updateHandler{c, root})
-	mux.Handle("/login", NewLoginHandler(c))
-	mux.Handle("/session-list", Auth(c, &sessionListHandler{s.tsp.Session}))
+
+	api := http.NewServeMux()
+	api.Handle("/login", NewLoginHandler(c))
+	api.Handle("/session-list", Auth(c, &sessionListHandler{s.tsp.Session}))
+	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", api))
+
 	if len(c.String(mino.KeyWebRoot)) > 0 {
 		log.Println("start web server with root", root)
 		mux.Handle("/", http.FileServer(http.Dir(root)))
 	}
+
 	return http.Serve(s.tsp.NetListener(), AccessLog(mux))
 }
