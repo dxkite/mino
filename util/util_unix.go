3440b4d931eebf6296ae44906893b3e540dd5bcb
dxkite <dxkite@163.com>
2021-08-05 22:26:32 +0800

    添加：获取本地请求的程序路径

---
+++ util/util_unix.go
@@ -0,0 +1,23 @@
+// +build !windows
+
+package util
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+func GetProgramByRemoteAddr(addr string) string {
+	cmd := exec.Command("netstat", "-anpt",
+		"|", "awk", "'{if($4 == \""+addr+"\"){print $7}}'",
+		"|", "awk", "-F", "'/'", "'{print $1}'",
+		"|", "xargs", "-I", "{}", "readlink /proc/{}/exe")
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Stderr = &buf
+	if err := cmd.Run(); err != nil {
+		return ""
+	} else {
+		return buf.String()
+	}
+}
---
+++ util/util_windows.go
@@ -0,0 +1,60 @@
+// +build windows
+
+package util
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+)
+
+func getPidByRemoteAddr(addr string) string {
+	cmd := exec.Command("netstat", "-ano", "-p", "tcp")
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Stderr = &buf
+
+	getPid := func(str string) string {
+		lns := strings.Split(str, "\n")
+		for _, ln := range lns {
+			txt := strings.Fields(ln)
+			if len(txt) > 1 && strings.Index(txt[1], addr) >= 0 {
+				return txt[4]
+			}
+		}
+		return ""
+	}
+
+	if err := cmd.Run(); err == nil {
+		if str := buf.String(); strings.Index(str, addr) >= 0 {
+			return getPid(str)
+		}
+	}
+	return ""
+}
+
+func getProgramByPid(pid string) string {
+	cmd := exec.Command("wmic", "process", "where", "processid="+pid, "get", "processid,executablepath,name")
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Stderr = &buf
+	if err := cmd.Run(); err != nil {
+		return ""
+	} else {
+		str := buf.String()
+		if strings.Index(str, pid) >= 0 {
+			lns := strings.Split(str, "\n")
+			fls := strings.Fields(lns[1])
+			return fls[0]
+		}
+	}
+	return ""
+}
+
+func GetProgramByRemoteAddr(addr string) string {
+	pid := getPidByRemoteAddr(addr)
+	if len(pid) > 0 {
+		return getProgramByPid(pid)
+	}
+	return ""
+}
