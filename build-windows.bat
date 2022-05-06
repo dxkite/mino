6ad9a932a4321eb3abdc597dacd8e0961ab82944
dxkite <dxkite@163.com>
2022-05-06 21:09:44 +0800

    feat: 发版支持压缩

--- build-windows.bat
+++ build-windows.bat
@@ -1,3 +1,4 @@
 FOR /F %%V IN ('git describe --tags') DO SET VERSION=%%V
 FOR /F %%V IN ('git rev-parse --short HEAD') DO SET COMMIT=%%V
 go build -o mino-%VERSION%-windows.exe -ldflags="-s -w -H windowsgui -X 'dxkite.cn/mino.Version=%VERSION%' -X 'dxkite.cn/mino.Commit=%COMMIT%'" ./cmd/mino
+7z a mino-%VERSION%-windows.exe.zip mino-%VERSION%-windows.exe
--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ if [[ $OSTYPE == darwin* ]]; then
     OS="macos"
 fi
 
-echo "build v$VERSION-$COMMIT-$OS"
+echo "build $VERSION-$COMMIT-$OS"
 go build -o "mino-$VERSION-$OS" -ldflags="-s -w -X 'dxkite.cn/mino.Version=$VERSION' -X 'dxkite.cn/mino.Commit=$COMMIT'" ./cmd/mino
+tar -cvzf mino-$VERSION-$OS.tar.gz mino-$VERSION-$OS
