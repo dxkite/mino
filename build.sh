79032331bc6cfe4df9186775df84c0bcc909035d
dxkite <dxkite@163.com>
2022-05-07 20:22:36 +0800

    feat: 优化构建脚本

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 VERSION=$(git describe --tags)
 COMMIT=$(git rev-parse --short HEAD)
-OS="linux"
 
-if [[ $OSTYPE == darwin* ]]; then
-    OS="macos"
-fi
+function build() {
+  OS=$1
+  ARCH=$2
+  NAME="mino-$VERSION-$OS-$ARCH"
+  LD_FLAG="-s -w -X 'dxkite.cn/mino.Version=$VERSION' -X 'dxkite.cn/mino.Commit=$COMMIT'"
+  if [[ $OS == windows* ]]; then
+      NAME="$NAME.exe"
+      LD_FLAG="-H windowsgui $LD_FLAG"
+  fi
+  echo "build $NAME@$COMMIT for $OS"
+  GOOS=$OS GOARCH=$ARCH go build -o "$NAME" -ldflags="$LD_FLAG" ./cmd/mino
+  tar -cvzf $NAME.tar.gz $NAME
+  echo "build $NAME success"
+}
 
-echo "build $VERSION-$COMMIT-$OS"
-go build -o "mino-$VERSION-$OS" -ldflags="-s -w -X 'dxkite.cn/mino.Version=$VERSION' -X 'dxkite.cn/mino.Commit=$COMMIT'" ./cmd/mino
-tar -cvzf mino-$VERSION-$OS.tar.gz mino-$VERSION-$OS
+build "windows" "amd64"
+build "windows" "386"
+build "linux" "amd64"
+build "linux" "386"
+build "darwin" "amd64"
+build "android" "arm64"
