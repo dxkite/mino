505ceb3f92add273e73150964ef6af3f6b92bb88
dxkite <dxkite@163.com>
2023-03-20 23:29:21 +0800

    chore: dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,6 @@ COPY mino /root/mino
 
 RUN chmod +x /root/mino && mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
 
-RUN echo 'address: ":28648"' >> /root/mino.yml
-RUN echo 'log_enable: false' >> /root/mino.yml
-
 WORKDIR /root
 
 EXPOSE 28648
---
+++ build-dev.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+VERSION=dev
+COMMIT=$(git rev-parse --short HEAD)
+COUNT=$(git rev-list HEAD --count)
+
+function build() {
+  OS=$1
+  ARCH=$2
+  NAME="mino-$VERSION.$COUNT-$OS-$ARCH"
+  LD_FLAG="-s -w -X 'dxkite.cn/mino.Version=$VERSION.$COUNT' -X 'dxkite.cn/mino.Commit=$COMMIT'"
+  if [[ $OS == windows* ]]; then
+      NAME="$NAME.exe"
+      LD_FLAG="-H windowsgui $LD_FLAG"
+  fi
+  echo "build $NAME@$COMMIT for $OS"
+  GOOS=$OS GOARCH=$ARCH go build -o "$NAME" -ldflags="$LD_FLAG" ./cmd/mino
+  tar -cvzf $NAME.tar.gz $NAME
+  echo "build $NAME success"
+}
+
+# function build_android() {
+#   git clone https://github.com/TTHHR/mino-android
+#   cp ./mino-$VERSION-android-arm64 ./mino-android/app/src/main/jniLibs/arm64-v8a/libmino.so
+#   cd mino-android
+#   chmod +x gradlew
+#   ./gradlew assembleDebug
+#   cp ./app/build/outputs/apk/debug/app-debug.apk ../mino-$VERSION-arm64-debug.apk
+# }
+
+build "linux" "amd64"
+build "linux" "386"
+build "darwin" "amd64"
